ad80ff04c0aa7dc57703946bd35be1a14f838197
bbarney7 <brennen.barney@gmail.com>
2017-01-23 12:58:36 -0700

    com

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,29 +12,49 @@ public class Main {
 		try {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-			Mat source = Highgui.imread("download.png", Highgui.CV_LOAD_IMAGE_COLOR);
-
+			Mat source = Highgui.imread("testImage1.jpg", Highgui.CV_LOAD_IMAGE_COLOR);
 			Mat destination = new Mat(source.rows(), source.cols(), source.type());
 			Imgproc.GaussianBlur(source, destination, new Size(45, 45), 0);
 			Highgui.imwrite("Gaussian45.jpg", destination);
+			medianBlur(55, source, destination);
 
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 
+		/*
+		 * try {
+		 * 
+		 * System.loadLibrary(Core.NATIVE_LIBRARY_NAME); Mat source =
+		 * Highgui.imread("Gaussian45.jpg", Highgui.CV_LOAD_IMAGE_COLOR); Mat
+		 * destination = new Mat(source.rows(), source.cols(), source.type());
+		 * 
+		 * destination = source; // Imgproc.threshold(source, destination, 128,
+		 * 255, // Imgproc.THRESH_BINARY); // Highgui.imwrite("ThreshZero.jpg",
+		 * destination); Imgproc.medianBlur(source, destination, 11);
+		 * Highgui.imwrite("MedianBlur.jpg", destination);
+		 * 
+		 * } catch (Exception e) { System.out.println("error: " +
+		 * e.getMessage()); }
+		 */
+	}
+
+	/**
+	 * @param k
+	 *            k is kernal size that we want to blur by
+	 * @param source
+	 * @param destination
+	 */
+	static void medianBlur(int k, Mat source, Mat destination) {
 		try {
 
-			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-			Mat source = Highgui.imread("Gaussian45.jpg", Highgui.CV_LOAD_IMAGE_COLOR);
-			Mat destination = new Mat(source.rows(), source.cols(), source.type());
-
-			destination = source;
-			Imgproc.threshold(source, destination, 75, 250, Imgproc.THRESH_BINARY);
-			Highgui.imwrite("ThreshZero.jpg", destination);
+			Imgproc.medianBlur(source, destination, k);
+			Highgui.imwrite("MedianBlur.jpg", destination);
 
 		} catch (Exception e) {
 			System.out.println("error: " + e.getMessage());
 		}
+
 	}
 
 }
