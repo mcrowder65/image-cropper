3c11393e2390c8bc9acff0f5cc96fc556f26f701
Eric <soreric94@gmail.com>
2017-01-25 11:39:19 -0700

    start main

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,14 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-			Mat source = Highgui.imread("testImage1.jpg", Highgui.CV_LOAD_IMAGE_COLOR);
+
+			if (args.length < 1) {
+				System.out.println("Usage: [input image name]");
+				return;
+			}
+			String imageName = args[0];
+
+			Mat source = Highgui.imread(imageName, Highgui.CV_LOAD_IMAGE_COLOR);
 			Mat destination = new Mat(source.rows(), source.cols(), source.type());
 			Imgproc.GaussianBlur(source, destination, new Size(45, 45), 0);
 			Highgui.imwrite("Gaussian45.jpg", destination);
