7ccf1bffa10b0d0c24dc3665a16eb36b177155ab
Eric <soreric94@gmail.com>
2017-01-30 12:22:03 -0700

    jioj

--- src/imageprimitives/MorphologicalOperations.java
+++ src/imageprimitives/MorphologicalOperations.java
@@ -1,17 +1,12 @@
 package imageprimitives;
 
 import generic.MatWrapper;
-import generic.Pixel;
 
 public class MorphologicalOperations {
 
 	public static MatWrapper dialate(MatWrapper input, int[][] mask, int onValue) {
 		MatWrapper output = new MatWrapper();
-		output = ColorOperations.toGrayscale(input);
-		output = ColorOperations.threshold(output);
 
-		@SuppressWarnings("unused")
-		Pixel p = input.getPixel(output.mat.height() / 2, output.mat.width() / 2);
 		return output;
 	}
 }
