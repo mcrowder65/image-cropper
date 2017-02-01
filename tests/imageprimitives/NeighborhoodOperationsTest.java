61dab0af42facaa951578bd1a4da644717ecc25a
bbarney7 <brennen.barney@gmail.com>
2017-02-01 12:11:35 -0700

    stash

--- tests/imageprimitives/NeighborhoodOperationsTest.java
+++ tests/imageprimitives/NeighborhoodOperationsTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opencv.core.Core;
 
-import generic.ImageComponent;
 import generic.MatWrapper;
 
 public class NeighborhoodOperationsTest {
@@ -25,65 +24,69 @@ public class NeighborhoodOperationsTest {
 		MatWrapper input = new MatWrapper(name + extension);
 		MatWrapper grayScaledImage = ColorOperations.toGrayscale(input);
 		MatWrapper threshImage = ColorOperations.threshold(grayScaledImage);
-		MatWrapper blurredImage = NeighborhoodOperations.medianBlur(85, threshImage);
-		ImageComponent comp = NeighborhoodOperations.connectedComponents(blurredImage);
-		MatWrapper maskedImage = NeighborhoodOperations.mask(comp, input);
-		maskedImage.Write(name + "Test" + extension);
+		MatWrapper blurredImage = NeighborhoodOperations.medianBlur(15, threshImage);
+		// MorphologicalOperations.
+		// ImageComponent comp =
+		// NeighborhoodOperations.connectedComponents(blurredImage);
+		// MatWrapper maskedImage = NeighborhoodOperations.mask(comp, input);
+		// maskedImage.Write(name + "Test" + extension);
+		// blurredImage.Write(name + "Test" + extension);
 	}
 
-	@Test
-	public void testCrop1() {
-		crop("familySearchImages/Crop1", ".jpg");
-	}
-
-	@Test
-	public void testCrop2() {
-		crop("familySearchImages/Crop2", ".jpg");
-	}
-
-	@Test
-	public void testCrop3() {
-		crop("familySearchImages/Crop3", ".jpg");
-	}
-
-	@Test
-	public void testCrop4() {
-		crop("familySearchImages/Crop4", ".jpg");
-	}
-
-	@Test
-	public void testCrop5() {
-		crop("familySearchImages/Crop5", ".jpg");
-	}
-
-	@Test
-	public void testCrop6() {
-		crop("familySearchImages/Crop6", ".jpg");
-	}
+	// @Test
+	// public void testCrop1() {
+	// crop("familySearchImages/Crop1", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop2() {
+	// crop("familySearchImages/Crop2", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop3() {
+	// crop("familySearchImages/Crop3", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop4() {
+	// crop("familySearchImages/Crop4", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop5() {
+	// crop("familySearchImages/Crop5", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop6() {
+	// crop("familySearchImages/Crop6", ".jpg");
+	// }
 
 	@Test
 	public void testCrop7() {
 		crop("familySearchImages/Crop7", ".jpg");
-	}
 
-	@Test
-	public void testCrop8() {
-		crop("familySearchImages/Crop8", ".jpg");
 	}
-
-	@Test
-	public void testCrop9() {
-		crop("familySearchImages/Crop9", ".jpg");
-	}
-
-	@Test
-	public void testCrop10() {
-		crop("familySearchImages/Crop10", ".jpg");
-	}
-
-	@Test
-	public void testCrop11() {
-		crop("familySearchImages/Crop11", ".jpg");
-	}
+	//
+	// @Test
+	// public void testCrop8() {
+	// crop("familySearchImages/Crop8", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop9() {
+	// crop("familySearchImages/Crop9", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop10() {
+	// crop("familySearchImages/Crop10", ".jpg");
+	// }
+	//
+	// @Test
+	// public void testCrop11() {
+	// crop("familySearchImages/Crop11", ".jpg");
+	// }
 
 }
