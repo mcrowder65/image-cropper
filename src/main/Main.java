8445948dad29bb4a60d064ffdfb7cee0965491f4
Matt Crowder <mcrowder65@gmail.com>
2017-02-08 12:45:03 -0700

    ABSTRACT!!

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,22 +26,49 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-			crop("familySearchImages/Crop1", ".jpg", 85);
-			crop("familySearchImages/Crop2", ".jpg", 85);
-			crop("familySearchImages/Crop3", ".jpg", 85);
-			crop("familySearchImages/Crop4", ".jpg", 85);
-			crop("familySearchImages/Crop5", ".jpg", 85);
-			crop("familySearchImages/Crop6", ".jpg", 85);
-			crop("familySearchImages/Crop7", ".jpg", 85);
-			crop("familySearchImages/Crop8", ".jpg", 85);
-			crop("familySearchImages/Crop9", ".jpg", 85);
-			crop("familySearchImages/Crop10", ".jpg", 85);
-			crop("familySearchImages/Crop11", ".jpg", 85);
+			// PUT FUNCTION HERE WHICH ONE YOU WANT TO RUN
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 
+	}
+
+	private static void runTeamAImages() {
+		crop("familySearchImages/Crop1", ".jpg", 85);
+		crop("familySearchImages/Crop2", ".jpg", 85);
+		crop("familySearchImages/Crop3", ".jpg", 85);
+		crop("familySearchImages/Crop4", ".jpg", 85);
+		crop("familySearchImages/Crop5", ".jpg", 85);
+		crop("familySearchImages/Crop6", ".jpg", 85);
+		crop("familySearchImages/Crop7", ".jpg", 85);
+		crop("familySearchImages/Crop8", ".jpg", 85);
+		crop("familySearchImages/Crop9", ".jpg", 85);
+		crop("familySearchImages/Crop10", ".jpg", 85);
+		crop("familySearchImages/Crop11", ".jpg", 85);
+	}
+
+	private static void runTeamBImages() {
+		crop("team b images/result1", ".jpg", 85);
+		crop("team b images/result10", ".jpg", 85);
+		crop("team b images/result11", ".jpg", 85);
+		crop("team b images/result2", ".jpg", 85);
+		crop("team b images/result3", ".jpg", 85);
+		crop("team b images/result4", ".jpg", 85);
+		crop("team b images/result5", ".jpg", 85);
+		crop("team b images/result6", ".jpg", 85);
+		crop("team b images/result7", ".jpg", 85);
+		crop("team b images/result8", ".jpg", 85);
+		crop("team b images/result9", ".jpg", 85);
+	}
+
+	private static void runTeamCImages() {
+		crop("Team c images/resulting_from_crop1", ".jpg", 85);
+		crop("Team c images/resulting_from_crop11", ".jpg", 85);
+		crop("Team c images/resulting_from_crop2", ".jpg", 85);
+		crop("Team c images/resulting_from_crop6", ".jpg", 85);
+		crop("Team c images/resulting_from_crop8", ".jpg", 85);
+		crop("Team c images/resulting_from_crop9", ".jpg", 85);
+		crop("Team c images/result2 copy", ".jpg", 85);
 	}
 
 }
